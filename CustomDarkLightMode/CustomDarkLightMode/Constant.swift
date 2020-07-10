504739d710944c56508cd2405c692bfc11caee04
Hao Lam <lamtuhao98@gmail.com>
2020-07-09 20:05:38 -0700

    Add new project

---
+++ CustomDarkLightMode/CustomDarkLightMode/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  CustomDarkLightMode
+//
+//  Created by Hao Lam on 7/8/20.
+//  Copyright © 2020 Hao Lam. All rights reserved.
+//
+
+import UIKit
+enum CustomColor
+{
+    static let customBackgroundColor = UIColor(named: "BackgroundColor")
+    static let customButtonColor = UIColor(named: "ButtonBackgroundColor")
+    static let customLabelColor = UIColor(named: "LabelColor")
+}
---
+++ CustomDarkLightMode/CustomDarkLightMode/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CustomDarkLightMode
+//
+//  Created by Hao Lam on 7/8/20.
+//  Copyright © 2020 Hao Lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = CustomColor.customBackgroundColor
+        
+    }
+
+
+}
+
